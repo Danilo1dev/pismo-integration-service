bab3e75a1b5fa6328d77c7423b7ee174f387f69a
Danilo1dev <danilofoxlog@gmail.com>
2024-10-24 15:34:50 -0300

    init config

---
+++ src/test/java/com/mentory/spring/pismo_integration_service/PismoIntegrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentory.spring.pismo_integration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PismoIntegrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
